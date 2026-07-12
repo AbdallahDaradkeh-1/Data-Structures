3f824536acb51743e1f89bacb63f14badee1c6db
AbdallahDaradkeh-1 <daradkehabdallah7@gmail.com>
2026-07-12 14:16:27 +0300

    Create Stack Data Structure without any operation yet

---
+++ Data Structure هياكل البيانات/Stack/createStack.py
@@ -0,0 +1,5 @@
+class stack:
+  def __init__(self):
+    self.s = []
+
+# Stack is like array, but we only can push, pop elements, get the top element(last element we enter) or check if the stack is empty
